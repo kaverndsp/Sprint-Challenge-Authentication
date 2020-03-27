2a258601d97d0f65ed48fb6a5db6ae1c6b0edfe3
kaverndsp <kaverndsp@users.noreply.github.com>
2020-03-27 11:14:11 -0500

    login endpoint tests done

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -4,14 +4,13 @@ const User = require("../users/users-model.js");
 const server = require("../api/server.js");
 
 
-describe("auth-router.js", function(){
+describe("auth-router.js", () => {
     describe("REGISTER", () => {
-        it("check is a user with the correct shape of data can register", async () => {
+        it("check if a user with the correct shape of data can register", async () => {
             const user = {
                 username: 'Daniel2',
                 password: 'password2'
             };
-            
             
             User.add(user);
 
@@ -29,8 +28,41 @@ describe("auth-router.js", function(){
                 .send(user);
             expect(res.status).toBe(201);
             });
+    
     });
 
+    describe("LOGIN", () => {
+        it('responds with 200', async () => {
+            const user = {
+                username: 'Daniel',
+                password: 'password'
+            };
+            await request(server)
+                .post('/api/auth/register')
+                .send(user);
+            
+            const res = await request(server)
+                .post('/api/auth/login')
+                .send(user);
+            
+            expect(res.status).toBe(200);
+    
+        });
 
+        it('server gives a 401 if credientials are invalid', async () => {
+            const user = {
+                username: 'NotDaniel',
+                password: 'notapassword'
+            };
+
+            const res = await request(server)
+                .post('/api/auth/login')
+                .send(user);
+            
+            expect(res.status).toBe(401);
+           
+        });
+    
+    });
 
 });
